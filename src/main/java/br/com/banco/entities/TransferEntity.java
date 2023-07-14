9b733b5987b034d20f0bb45cf74f782f5d787944
Diogo <diogodevweb@gmail.com>
2023-07-14 11:37:07 -0300

    refactore: cleaning unnecessary constructors

--- src/main/java/br/com/banco/entities/TransferEntity.java
+++ src/main/java/br/com/banco/entities/TransferEntity.java
@@ -3,7 +3,6 @@ package br.com.banco.entities;
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.Objects;
 
 @Entity
 @Table(name = "transferencia")
@@ -30,75 +29,24 @@ public class TransferEntity {
     @JoinColumn(name = "conta_id", nullable = false)
     private AccountEntity account;
 
-    public TransferEntity() {
-    }
-
-    public TransferEntity(Long id, LocalDateTime transferDate, BigDecimal value, String type, String transactionOperatorName, AccountEntity account) {
-        this.id = id;
-        this.transferDate = transferDate;
-        this.value = value;
-        this.type = type;
-        this.transactionOperatorName = transactionOperatorName;
-        this.account = account;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public LocalDateTime getTransferDate() {
         return transferDate;
-    }
-
-    public void setTransferDate(LocalDateTime transferDate) {
-        this.transferDate = transferDate;
     }
 
     public BigDecimal getValue() {
         return value;
     }
 
-    public void setValue(BigDecimal value) {
-        this.value = value;
-    }
-
     public String getType() {
         return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
     }
 
     public String getTransactionOperatorName() {
         return transactionOperatorName;
     }
 
-    public void setTransactionOperatorName(String transactionOperatorName) {
-        this.transactionOperatorName = transactionOperatorName;
-    }
-
     public AccountEntity getAccount() {
         return account;
     }
 
-    public void setAccount(AccountEntity account) {
-        this.account = account;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof TransferEntity that)) return false;
-        return Objects.equals(getId(), that.getId());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId());
-    }
 }
