77ace995dc1dc74d7d4732cec38628a07f64cf53
Diogo <diogodevweb@gmail.com>
2023-07-14 10:24:32 -0300

    feat:
    return all transfers if no filter is given

--- src/main/java/br/com/banco/services/ListTransferDataService.java
+++ src/main/java/br/com/banco/services/ListTransferDataService.java
@@ -31,7 +31,7 @@ public class ListTransferDataService {
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
 
         BigDecimal balanceInPeriod = transferDTOs.stream()
-                .filter(transferDTO -> transferDTO.getTransferDate().isAfter(startDate) && transferDTO.getTransferDate().isBefore(endDate))
+                .filter(transferDTO -> isWithinPeriod(transferDTO.getTransferDate(), startDate, endDate))
                 .map(TransferDTO::getValue)
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
 
@@ -40,13 +40,30 @@ public class ListTransferDataService {
             transferDTO.setBalanceInPeriod(balanceInPeriod);
         });
 
+        if (startDate == null && endDate == null && transactionOperatorName == null && type == null) {
+            return transferDTOs;
+        }
 
         return transferDTOs.stream()
-                .filter(transferDTO -> transferDTO.getTransferDate().isAfter(startDate) && transferDTO.getTransferDate().isBefore(endDate))
-                .filter(transferDTO -> transferDTO.getTransactionOperatorName().toUpperCase().equals(transactionOperatorName.toUpperCase()))
-                .filter(transferDTO -> transferDTO.getType().toUpperCase().equals(type.toUpperCase()))
+                .filter(transferDTO -> startDate == null || isAfterOrEqual(transferDTO.getTransferDate(), startDate))
+                .filter(transferDTO -> endDate == null || isBeforeOrEqual(transferDTO.getTransferDate(), endDate))
+                .filter(transferDTO -> transactionOperatorName == null || transferDTO.getTransactionOperatorName() != null && transferDTO.getTransactionOperatorName().toUpperCase().equals(transactionOperatorName.toUpperCase()))
+                .filter(transferDTO -> type == null || transferDTO.getType() != null && transferDTO.getType().toUpperCase().equals(type.toUpperCase()))
                 .collect(Collectors.toList());
     }
+
+    private boolean isWithinPeriod(LocalDateTime date, LocalDateTime startDate, LocalDateTime endDate) {
+        return (startDate == null || isAfterOrEqual(date, startDate)) && (endDate == null || isBeforeOrEqual(date, endDate));
+    }
+
+    private boolean isAfterOrEqual(LocalDateTime date1, LocalDateTime date2) {
+        return date1 != null && (date1.isEqual(date2) || date1.isAfter(date2));
+    }
+
+    private boolean isBeforeOrEqual(LocalDateTime date1, LocalDateTime date2) {
+        return date1 != null && (date1.isEqual(date2) || date1.isBefore(date2));
+    }
 }
+
 
 
